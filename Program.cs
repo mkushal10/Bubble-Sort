e4ae9e5a29463bc6a7f1f1db90de26a20d48f0bf
Kushal Neupane <mkushal10@gmail.com>
2021-02-12 18:34:44 +0545

    1st commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[55];
+            int i, j, n, temp;
+
+            Console.Write("Enter the Size of Array : ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Array Elements : ");
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse((Console.ReadLine()));
+            }
+            for (i = 0; i < (n - 1); i++)
+            {
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Bubble Sort (Ascending) :");
+            for (i = 0; i < n; i++)
+                Console.WriteLine(arr[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
